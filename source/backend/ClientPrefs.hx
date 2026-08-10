8ff59732a9afbe4c7957232e31e45a73b7568914
Unluckystill <mvorezzm@gmail.com>
2026-08-10 18:42:21 +0300

    ...

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -16,6 +16,7 @@ import states.TitleState;
 	public var screensaver:Bool = false;
 	public var wideScreen:Bool = false;
 	public var hitboxType:String = "Gradient";
+	public var vsliceMobileControls:Bool = false; // V-Slice style centered arrow hitboxes
 	public var popUpRating:Bool = true;
 	public var vsync:Bool = false;
 	public var gameOverVibration:Bool = false;
