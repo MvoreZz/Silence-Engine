1019e9e2385ca4f42ca6abceaed0c48f4554358b
MvoreZz <mvorezzm@gmail.com>
2026-08-26 08:58:04 +0300

    Idk

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -132,14 +132,10 @@ class MainMenuState extends MusicBeatState
 			}
 		}
 
-		var engineVer:FlxText = new FlxText(12, FlxG.height - 64, 0, silenceEngineName + " v" + silenceEngineVersion, 12);
+		var engineVer:FlxText = new FlxText(12, FlxG.height - 44, 0, silenceEngineName + " v" + silenceEngineVersion, 12);
 		engineVer.scrollFactor.set();
 		engineVer.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(engineVer);
-		var psychVer:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
-		psychVer.scrollFactor.set();
-		psychVer.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(psychVer);
 		var fnfVer:FlxText = new FlxText(12, FlxG.height - 24, 0, "Friday Night Funkin' v" + Application.current.meta.get('version'), 12);
 		fnfVer.scrollFactor.set();
 		fnfVer.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
